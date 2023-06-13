7171223668e770ff02cd1e845cf792738abc3c99
jamiespicer-demonbaby <136043579+jamiespicer-demonbaby@users.noreply.github.com>
2023-06-14 10:36:47 +1200

    Add files via upload

---
+++ 01_yes_no_v1.py
@@ -0,0 +1,13 @@
+# functions
+
+
+
+# main routine
+want_instructions = input("Do you want instructions? ")
+
+if want_instructions == "yes":
+    print("Instructions go here")
+elif want_instructions == "no":
+    pass
+else:
+    print("please answer yes / no")
