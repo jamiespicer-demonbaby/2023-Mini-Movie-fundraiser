0627cd06efe8152b60b2de052c01a012b69aa6e4
jamiespicer-demonbaby <136043579+jamiespicer-demonbaby@users.noreply.github.com>
2023-07-19 10:06:46 +1200

    Add files via upload

---
+++ 07_string_checker.py
@@ -0,0 +1,9 @@
+# checks user entered valid response
+# cash / credit based on list of options
+def string_checker(questions, num_letters, valid_response):
+    pass
+
+
+# main routine
+yes_no_list = ["yes", "no"]
+payment_list = ["cash", "credit"]
